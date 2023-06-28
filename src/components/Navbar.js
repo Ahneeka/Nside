49e5db70581073c585f6274611ffeeb028472a56
nnjokunneka111@gmail.com <nnjokunneka111@gmail.com>
2023-06-29 00:24:03 +0100

    save

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./styles/Navbar.css"
+
+function Navbar() {
+  return (
+    <div className='sum'>
+        <div className='logo'>
+            Nside
+        </div>
+        <nav className=''>
+            <ul className=''>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+
+                <li>
+                    <Link to="/careers">Careers</Link>
+                </li>
+
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+
+                <li>
+                    <Link to="/projects">Projects</Link>
+                </li>
+
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+
+
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
