80853d148909282309d5c34301dfd16bda666327
nnjokunneka111@gmail.com <nnjokunneka111@gmail.com>
2023-06-29 02:24:37 +0100

    save

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 
 import './App.css';
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Careers from './pages/Careers';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"  element={<Navbar/>} />
+          <Route path="/careers"  element={<Careers/>} />
+          <Route path="/about"  element={<About/>} />
+          <Route path="/projects"  element={<Projects/>} />
+          <Route path="/contact"  element={<Contact/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
